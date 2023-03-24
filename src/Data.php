9451cfb33d01e264bc3cbb8a361f896e565f5e46
mzhaaf <hafizhatul.kiromi@sirclo.com>
2023-03-24 09:44:17 +0800

    initial commit

---
+++ src/Data.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Omi\Helpers;
+
+use DB;
+
+class Data
+{
+  public static function getDimension($length, $depth, $height) {
+    
+    $dimensions = [];
+
+    if(!empty($length)) array_push($dimensions, "W" . $length);
+    if(!empty($depth)) array_push($dimensions, "D" . $depth);
+    if(!empty($height)) array_push($dimensions, "H" . $height);
+    $str_dimensions = implode(" x ", $dimensions);
+
+    return $str_dimensions;
+
+  }
+}
